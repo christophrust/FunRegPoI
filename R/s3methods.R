4b688ad7de691cb18b8ce951a2d9e5e0079ad5ef
christophrust <christoph.g.rust@googlemail.com>
2019-01-29 18:32:53 +0100

    CI also for KPS estimator

--- R/s3methods.R
+++ R/s3methods.R
@@ -96,8 +96,22 @@ summary.FunRegPoI <- function(object, confidence.level = 0.95,...){
         XtX1_Xt <- object$model$XtX_1Xt
         s2.resid <- sum(object$residuals^2)/( length(object$residuals) - eff.df )
         coef.serr.ptw  <- sqrt(diag(tcrossprod(XtX1_Xt)) * s2.resid )
-        
-        if ( (length(coefs$betaAddVar) + length(coefs$betaPoI))>0){
+    } else {
+        s2.resid <- sum(object$residuals^2)/( length(object$residuals) - eff.df )
+        Xtau <- t(object$data$X_mat[ coefs$tauInd,] -
+                  rowMeans(object$data$X_mat[ coefs$tauInd,]))  
+        Xi <- obj$model$estScores[,1:obj$model$K, drop = FALSE]
+        Lambda <- if (is.matrix(efs <- obj$model$estEigenFct))
+                      obj$model$estEigenFct else matrix(efs, ncol = 1)
+        PreMat <- matrix(0, nrow(Lambda) + ncol(Xtau), obj$model$K + ncol(Xtau))
+        PreMat[1:nrow(Lambda), 1:ncol(Lambda)] <- Lambda
+        PreMat[nrow(Lambda)+1:ncol(Xtau), object$model$K + 1:ncol(Xtau)] <- diag(ncol(Xtau))
+        coef.serr.ptw  <- sqrt(diag(PreMat %*% chol2inv( chol( crossprod( cbind(Xi, Xtau)))) %*%
+                                    t(PreMat))) * s2.resid
+            
+    }
+    
+    if ( (length(coefs$betaAddVar) + length(coefs$betaPoI))>0){
 
             ## PoI coef and AddVar coef summaries
             coef <- matrix(NA , ncol = 4, nrow = length(coefs$betaPoI) + length(coefs$betaAddVar))
@@ -109,30 +123,26 @@ summary.FunRegPoI <- function(object, confidence.level = 0.95,...){
             coef[,3] <- coef[,1] / coef[,2]
             coef[,4] <- 1 - pnorm( abs( coef[,3]))
 
-            obj <- c(obj , list(coefficients = coef))
-        } else {
-            obj <- c(obj , list(coefficients = NULL))
-        }
-        
-        
-        
-        ## beta curve confidence interval
-        
-        q.norm <-  qnorm(1 - (1- confidence.level)/2 )
-        confint.beta <- cbind(lower = estBeta - q.norm * coef.serr.ptw[1:length(estBeta)],
-                              upper = estBeta + q.norm * coef.serr.ptw[1:length(estBeta)])
-
-        alpha.bf <- (1- confidence.level)/length(estBeta)
-        q.norm.bf <-  qnorm(1-alpha.bf/2 )
-        confint.beta.bf <- cbind(lower = estBeta - q.norm.bf * coef.serr.ptw[1:length(estBeta)],
-                                 upper = estBeta + q.norm.bf * coef.serr.ptw[1:length(estBeta)])
-        
-        obj <- c(obj , list( confint.beta = list(classic = confint.beta, bonferroni = confint.beta.bf)))
-        
+        obj <- c(obj , list(coefficients = coef))
     } else {
-        obj <- c(obj , list(betaPoI = betaPoI,
-                            betaAddVar = betaAddVar))
+        obj <- c(obj , list(coefficients = NULL))
     }
+        
+        
+        
+    ## beta curve confidence interval
+    
+    q.norm <-  qnorm(1 - (1- confidence.level)/2 )
+    confint.beta <- cbind(lower = estBeta - q.norm * coef.serr.ptw[1:length(estBeta)],
+                          upper = estBeta + q.norm * coef.serr.ptw[1:length(estBeta)])
+    
+    alpha.bf <- (1- confidence.level)/length(estBeta)
+    q.norm.bf <-  qnorm(1-alpha.bf/2 )
+    confint.beta.bf <- cbind(lower = estBeta - q.norm.bf * coef.serr.ptw[1:length(estBeta)],
+                             upper = estBeta + q.norm.bf * coef.serr.ptw[1:length(estBeta)])
+    
+    obj <- c(obj , list( confint.beta = list(classic = confint.beta, bonferroni = confint.beta.bf)))
+    
 
     class(obj) <- "summary.FunRegPoI"
     obj
@@ -156,23 +166,13 @@ print.summary.FunRegPoI <-function(x , signif.stars = TRUE,digits = 3,...){
         if (length(taus)>0 || length(x$betaAddVar) > 0){ 
             cat("\n\nPoint of Impact location(s) (index) in [0,1]: ")
             cat(paste0(round(taus , digits = 2) , " (" , x$tausInd , ")" , collapse = " "))
-            if (x$call != "KPS"){
-                cat(paste0("\n\nPoint of Impact influences",
-                           if ( length(x$betaAddVar) > 0 ) " and additional Variables:\n\n" else ":\n\n"))
-                printCoefmat(coefs, digits = digits, signif.stars = signif.stars, 
-                             na.print = "NA", ...)
-            } else {
-                cat("\nPoint of Impact influences: ")
-                cat(format(x$betaPoI , digits = 3))
-                
-                if (lenCoef <- length(x$betaAddVar) > 0 ){
-                    cat("\n Influence of additional variables:")
-                    for (i in 1:lenCoef) {
-                        cat(paste0("\n", names(x$betaAddVar)[i] , ": " , round(x$betaAddVar, digits=3)))
-                    }
-                }
-            }
+            
+            cat(paste0("\n\nPoint of Impact influences",
+                       if ( length(x$betaAddVar) > 0 ) " and additional Variables:\n\n" else ":\n\n"))
+            printCoefmat(coefs, digits = digits, signif.stars = signif.stars, 
+                         na.print = "NA", ...)
         }
+        
         
         cat(paste0("\nBIC: " , round(bic,digits = 3)))
         cat("\nOptimal k (delta): ")
@@ -195,15 +195,11 @@ print.summary.FunRegPoI <-function(x , signif.stars = TRUE,digits = 3,...){
 #' @export
 plot.summary.FunRegPoI <-function(x,bonferroni = TRUE,...){
     
-    if (x$call != "KPS"){
-        beta.rng  <-range(x$confint.beta) + diff(range(x$confint.beta)) * c(-0.2,+0.2)
-        estBeta <- x$estBeta
-    } else {
-        beta.rng  <-range(estBeta <- x$estBeta) + diff(range(x$estBeta)) * c(-0.2,+0.2)
-    }
+    beta.rng  <-range(x$confint.beta) + diff(range(x$confint.beta)) * c(-0.2,+0.2)
+    estBeta <- x$estBeta
     taus <- x$taus
     if (length(taus)>0) {
-        PoIcoefs <- if (x$call != "KPS") x$coefficients[1:length(taus),1] else x$betaPoI
+        PoIcoefs <- x$coefficients[1:length(taus),1]
     }
     ylims <- range(estBeta) + diff(range(estBeta)) * c(-0.2,+0.2)
     
@@ -211,14 +207,14 @@ plot.summary.FunRegPoI <-function(x,bonferroni = TRUE,...){
     plot(y=beta.rng , x=c(0,1) , col="white" ,xaxt="n", xlab= "grd", ylab ="beta",...)
 
     ## plot the pointwise confidence interval:
-    if (x$call != "KPS"){
-        grd <- seq(0,1, len = length(estBeta))
-        
-        lwAndUp <-  if (bonferroni) x$confint.beta$bonferroni else x$confint.beta$classic
-        y.ci <- c( lwAndUp[,"lower"], lwAndUp[length(estBeta):1,"upper"])
-        x.ci <- c(grd, grd[length(estBeta):1])
-        polygon(x = x.ci, y = y.ci, col = "lightgrey", border = NA)
-    }
+    
+    grd <- seq(0,1, len = length(estBeta))
+    
+    lwAndUp <-  if (bonferroni) x$confint.beta$bonferroni else x$confint.beta$classic
+    y.ci <- c( lwAndUp[,"lower"], lwAndUp[length(estBeta):1,"upper"])
+    x.ci <- c(grd, grd[length(estBeta):1])
+    polygon(x = x.ci, y = y.ci, col = "lightgrey", border = NA)
+    
     
     ## Abline grey PoI lines
     if (length(taus)>0){
